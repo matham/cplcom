92216b47707fecd2d7c853359c805b2abdfb623e
Matthew Einhorn <moiein2000@gmail.com>
2015-02-02 16:30:19 -0500

    Add kv loading and data files to setup.

--- cplcom/__init__.py
+++ cplcom/__init__.py
@@ -1,2 +1,10 @@
 
 __version__ = '0.1-dev'
+
+from os.path import join, dirname
+
+from kivy.resources import resource_add_path
+from kivy.lang import Builder
+
+resource_add_path(join(dirname(dirname(__file__)), 'media'))
+Builder.load_file(join(dirname(__file__), 'graphics.kv'))
