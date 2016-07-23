027ea2c96a213e1e2942d6d514f9fada2abc9884
Matthew Einhorn <moiein2000@gmail.com>
2016-07-22 22:19:09 -0400

    Bump version to v0.1.1.dev0.

--- cplcom/__init__.py
+++ cplcom/__init__.py
@@ -4,7 +4,7 @@
 A project that collects various tools and utilities used in the CPL lab.
 '''
 
-__version__ = '0.1'
+__version__ = '0.1.1.dev0'
 
 config_name = 'CPLCom'
 '''The config parser name used when storing CPLCom related configuration data.
